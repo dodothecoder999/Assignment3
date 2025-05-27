84621b16af9d89ed309b1a61efecb820a5fd8e2b
saniya koyal <dodoprogramming019@gmail.com>
2025-05-27 19:40:08 +0530

    Add files via upload

---
+++ 3task2.py
@@ -0,0 +1,9 @@
+import math
+a=float(input("Enter a number:"))
+sqrt_a=math.sqrt(a)
+log_a=math.log(a)
+sin_a=math.sin(a)
+
+print(f"The square root of {a} is {sqrt_a}")
+print(f"The logarithm root of {a} is {log_a}")
+print(f"The sine root of {a} is {sin_a}")
