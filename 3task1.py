42fae0e26485d2a0c6f933fbe4901efd2cbd26eb
saniya koyal <dodoprogramming019@gmail.com>
2025-05-28 19:19:35 +0530

    Delete 3task1.py

--- 3task1.py
+++
@@ -1,9 +0,0 @@
-#Factorial using function
-
-def factorial(n):
-    if n==0 or n==1:
-        return
-    else:
-        return n*Factorial(n-1)
-num=int(input("Enter a number:"))
-print(F"Factorial of {num}is {factorial(num)}")
