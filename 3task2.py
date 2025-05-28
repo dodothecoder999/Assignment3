ca21e48aefcc4d69a9df1bd06ae9d56e02da2a5d
saniya koyal <dodoprogramming019@gmail.com>
2025-05-28 21:20:01 +0530

    Update 3task2.py

--- 3task2.py
+++ 3task2.py
@@ -4,14 +4,14 @@ sqrt_a=math.sqrt(a)
 log_a=math.log(a)
 sin_a=math.sin(a)
 
-print(f"The square root of {a} is {sqrt_a}")
-print(f"The logarithm root of {a} is {log_a}")
-print(f"The sine root of {a} is {sin_a
+print("The sqaure root is",sqrt_a)
+print("The logarithm is",log_a)
+print("The sine is",sin_a)
 
 OUTPUT
 
  PS C:\Users\SANIYA\OneDrive\Desktop\dodothecoder> & "C:/Program Files/Python313/python.exe" c:/Users/SANIYA/OneDrive/Desktop/dodothecoder/3task2.py
-Enter a number:45
-The square root of 45.0 is 6.708203932499369
-The logarithm root of 45.0 is 3.8066624897703196
-The sine root of 45.0 is 0.8509035245341184                               
+Enter a number:9
+The sqaure root is 3.0
+The logarithm is 2.1972245773362196
+The sine is 0.4121184852417566
